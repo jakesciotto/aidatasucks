ecb231ce37a60a3ec66dcf6147206116860d20ea
Jake Sciotto <jake.sciotto@gmail.com>
2026-02-25 14:20:59 -0700

    update

--- instrumentation-client.js
+++ instrumentation-client.js
@@ -2,7 +2,7 @@ import posthog from "posthog-js";
 
 posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
   // api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-  api_host: "https://analytics.aidatasucks.com",
+  api_host: "https://app.aidatasucks.com",
   defaults: "2026-01-30",
   capture_exceptions: true,
   debug: process.env.NODE_ENV === "development",
