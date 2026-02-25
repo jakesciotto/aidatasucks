149f40c90ca4ef51011ebed2b8d5ba4e6d965a5e
Jake Sciotto <jake.sciotto@gmail.com>
2026-02-25 14:36:09 -0700

    vercel updates? dns and stuff is hard

--- instrumentation-client.js
+++ instrumentation-client.js
@@ -1,8 +1,8 @@
 import posthog from "posthog-js";
 
 posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
-  // api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-  api_host: "https://app.aidatasucks.com",
+  api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+  ui_host: "https://us.posthog.com",
   defaults: "2026-01-30",
   capture_exceptions: true,
   debug: process.env.NODE_ENV === "development",
