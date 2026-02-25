0186ee50bd6c6a2d0d4edf76b7c2214121a73a9c
Jake Sciotto <jake.sciotto@gmail.com>
2026-02-25 14:19:40 -0700

    vercel update

--- instrumentation-client.js
+++ instrumentation-client.js
@@ -1,7 +1,8 @@
 import posthog from "posthog-js";
 
 posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
-  api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+  // api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+  api_host: "https://analytics.aidatasucks.com",
   defaults: "2026-01-30",
   capture_exceptions: true,
   debug: process.env.NODE_ENV === "development",
