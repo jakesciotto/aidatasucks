d54a9033124082365141f190316136cf412b3d4e
Jake Sciotto <jake.sciotto@gmail.com>
2026-04-14 09:48:58 -0600

    ph person profiles

--- instrumentation-client.js
+++ instrumentation-client.js
@@ -3,6 +3,9 @@ import posthog from "posthog-js";
 posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
   api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
   ui_host: "https://us.posthog.com",
+  person_profiles: "always",
+  capture_pageview: true,
+  capture_pageleave: true,
   capture_exceptions: true,
   debug: process.env.NODE_ENV === "development",
 });
